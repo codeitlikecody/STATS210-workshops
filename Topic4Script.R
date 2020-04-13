4a3f95580e79cbff07ab1bf8e946d42e85319497
Chris Cody <3807221+aussiechris@users.noreply.github.com>
2020-04-13 15:21:54 +1000

    Finished Prac 4

--- Topic4Script.R
+++ Topic4Script.R
@@ -7,7 +7,7 @@
 
 # set no of significant figures and hide significance
 # stars from output
-options(digits = 6, show.signif.stars = F)
+options(digits = 6, show.signif.stars = T)
 
 # import STAT210 functions script
 source("Rfunctions.R")
@@ -21,7 +21,7 @@ library(lattice)
 # load data
 samara.df <- read.table("samara.txt", header = T)
 
-#plot data
+# plot data
 samaraEDA <- xyplot(Velocity ~ Load | Tree,
   data = samara.df, layout = c(3, 1),
   panel = function(x, y) {
@@ -31,7 +31,66 @@ samaraEDA <- xyplot(Velocity ~ Load | Tree,
 )
 print(samaraEDA)
 
-factor(samara.df$Tree)
+# declare Tree as a factor
+# class(samara.df$Tree)
+# levels(samara.df$Tree)
+samara.df$Tree <- factor(samara.df$Tree)
+# class(samara.df$Tree)
+# levels(samara.df$Tree)
+
+# Fit three models
+# Interaction model
+samara.mod1 <- lm(Velocity ~ Load * Tree, data = samara.df)
+anova(samara.mod1)
+# Main effects model
+samara.mod2 <- lm(Velocity ~ Load + Tree, data = samara.df)
+anova(samara.mod2)
+# Simple linear regression model
+samara.mod3 <- lm(Velocity ~ Load, data = samara.df)
+anova(samara.mod3)
+
+# compare models
+anova(samara.mod1, samara.mod2, samara.mod3)
+
+# stepwise regression: forward
+samara.form_l <- formula(~1)
+samara.form_u <- formula(~ Load * Tree, data = samara.df)
+
+start.model <- lm(Velocity ~ 1, data = samara.df)
+stepf.model <- step(
+  start.model,
+  direction = "forward",
+  scope = list(
+    lower = samara.form_l,
+    upper = samara.form_u
+  )
+)
+
+summary(stepf.model)
+
+# stepwise regression: backward
+
+startb.model <- lm(Velocity ~ Load * Tree, data = samara.df)
+stepb.model <- step(
+  startb.model,
+  direction = "backward",
+  scope = list(
+    lower = samara.form_l,
+    upper = samara.form_u
+  )
+)
+
+summary(stepb.model)
+
+anova(stepb.model)
+
+betaCI(stepb.model)
+
+#diagnostics plots for interaction model
+par(mfrow=c(1,2))
+plot(stepb.model,which=1:2)
+
 
 # delete files for this prac
-rm(samara.df, samaraEDA)
+rm(list=ls(pattern="samara"))
+rm(list=ls(pattern="st"))
